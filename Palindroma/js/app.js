8ec0d682de054890e93d2e1152239638170ad8c2
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-02-28 18:15:15 +0100

    feat: function polindrome-word created

--- Palindroma/js/app.js
+++ Palindroma/js/app.js
@@ -1,19 +1,21 @@
 
-
+//creo un prompt dove chiedo all'utente di inserire una parola
 const userWord = prompt('Inserisci una parola:');
 
-let reverseUserWord = '';
-for (i = userWord.length - 1; i >= 0; i--){
-     reverseUserWord += userWord[i];
-} 
-console.log(reverseUserWord);
-
-if (reverseUserWord === userWord){
+//chiedo se la parola sia palindroma o meno invocando la funzione creata in un if/else
+if (polindromeWord(userWord)) {
     console.log('La parola è palindroma');
 } else {
     console.log('La parola non è palindroma');
 }
 
+//creo una funzione con un ciclo for per ricreare la parola immessa al contrario e determinarne un confronto
+function polindromeWord(wordInput) {
+    let reverseUserWord = '';
+    for (i = userWord.length - 1; i >= 0; i--) {
+        reverseUserWord += userWord[i];
+    }
+    console.log(reverseUserWord);
 
-
-
+    return reverseUserWord === userWord;
+}
