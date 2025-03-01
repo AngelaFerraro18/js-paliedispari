1f0d0c81e367d8bda4dc13022d7064861f693a17
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-03-01 11:48:07 +0100

    feat: create function to check if a word is polindrome or not

--- Palindroma - soluzione 2/js/app-2.js
+++ Palindroma - soluzione 2/js/app-2.js
@@ -2,13 +2,21 @@
 //creo un prompt dove chiedo all'utente di inserire una parola
 const userWord = prompt('Inserisci una parola:');
 
-let isPalindrome = true;
-
-for (i = 0; i < userWord.length/2; i++){
-    if(userWord[i] !== userWord[userWord.length-1-i]){
-        isPalindrome = false;
-        break;
-    }
+//creo una funzione per determinare che una parola sia palindroma o no
+function isWordPalindrome (wordInputUser){
+    let isPalindrome = true;
+    for (i = 0; i < userWord.length/2; i++){
+        if(userWord[i] !== userWord[userWord.length-1-i]){
+            isPalindrome = false;
+            break;
+        }
+    } 
+    return isPalindrome;
 }
-console.log(isPalindrome);
 
+//se la parola risulta palidroma, verrà stampata la parola altrimenti stamperà un messaggio di avviso del contrario
+if(isWordPalindrome(userWord)){
+    console.log(`La parola è palindroma: ${userWord}`);
+} else {
+    console.log('La parola non è palindroma.')
+}
